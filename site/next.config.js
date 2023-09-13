5898364f98a05298b7d2050ca9e7000caa2b19ac
Kurt Malley <kjmalley7@gmail.com>
2023-09-12 14:49:11 -1000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'kmal808 Registry',
+    description: 'Curated registry of custom KASM workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://kmal808.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/kmal808/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
